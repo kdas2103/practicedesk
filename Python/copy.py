452bfbeab689dc67abec288f01f5d156fe5594e6
kdas2103 <kaustavdas2103@gmail.com>
2020-09-25 09:21:06 +0530

    commit01

---
+++ Python/copy.py
@@ -0,0 +1,16 @@
+#import os                                               # importing OS module
+#print (os.getcwd())                                     # printing current working directory
+#os.chdir("D:\\delaPlex Policy Documents")               # changing the directory    
+#os.mkdir("D:\\delaPlex Policy Documents\\testdir")      # making a directory at location specified
+#os.rmdir("D:\\delaPlex Policy Documents\\testdir")      # deleting a directory from a location specified to delete file use os.remove
+
+#print (os.getcwd())
+#import sys
+
+#print-- that give sus the output syntax= print (variable)-- Variables in Python are case sensetive 
+# string is an line/ word etc
+
+x= "India"
+#print(x.upper()) G6:G16
+px[1:3] # remeber the last digit will not be included
+
---
+++ Python/firstpythonfile.py
@@ -0,0 +1,46 @@
+#name='kaustav'
+Movies=["bala",'Uri',102]
+#print(Movies)
+#Movies.append("netflix")
+#print(Movies)
+#movies="SunnyL"
+# print(movies)
+#movies.insert(2, 108)
+#print(movies)
+#movies.remove (105)
+#print (movies)
+#movies.reverse()
+#print (movies)
+#name= "India"
+#print (name[2])
+#print (name[-2])
+#print(movies[3])
+
+numbers=[0,10,102]
+print(numbers.reverse())
+
+
+animals= {
+           "reptiles": "snake",
+           "canniens": "Dog",
+           'birds': 'Parrot'
+}
+
+#print(animals)
+#print(animals.get('birds'))
+#(animals.add['insects']='ant')
+#print(animals)
+
+my_name=input ("what is your name?" )
+print (my_name)
+
+
+cars = {
+        'Mercedes': {'Color': 'Black', 'year': "1970"},
+        'Ford': {'Color': 'White', 'year': '1998'},
+        'Honda': {'Color': 'Grey', 'year': '2019'},
+        'Maruti': {'Color': 'Blue', 'year': '1992'},
+        'Ferrari': {'Color': 'Red', 'year': '2020'}
+        }
+
+#print(cars)
